1fe1ffaf0f181ef8c23e33e7deb03f2e8d5dcf55
John Doe <elliehachemjob@gmail.com>
2022-10-20 15:12:51 +0300

    dynamic-sub-categories

--- src/app/pages/sub-categories/sub-categories.component.ts
+++ src/app/pages/sub-categories/sub-categories.component.ts
@@ -13,7 +13,8 @@ export class SubCategoriesComponent implements OnInit {
   //variable sections
   isHidden: boolean = true;
   isLogoActive: boolean = false;
-
+  subCategoryName: string = "Impulse";
+  subCategories: any = [{ title: "Impulse A", subTitle: "1 shelf - main placement", nestedDetails: { title: "12 Facings", imagePath: "https://wingmanapi.rbprojects.me/public/uploads/product/1630222037.png", flow: "Traffic Flow", isEmptyContent: false } }, { title: "Impulse B", subTitle: "1 shelf - main placement", nestedDetails: { title: "", imagePath: "", flow: "", isEmptyContent: true } }, { title: "Impulse C", subTitle: "1 shelf - main placement", nestedDetails: { title: "", imagePath: "", flow: " Flow", isEmptyContent: true } }]
 
   // Injection dependencies If needed + 
   constructor(
